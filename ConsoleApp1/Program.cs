6741b3c3cd4477de282186d7077f907bce1df16b
Kieran <73611627+althruist@users.noreply.github.com>
2024-10-03 11:00:47 +0200

    Thu 3 Oct - Learning C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int generatedNumber;
+
+Random generator = new Random();
+generatedNumber = generator.Next(1, 11);
+
+Console.WriteLine(generatedNumber);
+
+Console.WriteLine("Choose from 1-10");
+
+string userInput = Console.ReadLine();
+
+int userInputInt = Convert.ToInt32(userInput);
+
+if (userInputInt == generatedNumber)
+    Console.WriteLine("YOU DID IT!!!!!!");
+else
+    Console.WriteLine("you suck.");
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿string word = "WORD!";
+
+Console.WriteLine("Remember this word: " + word);
+Thread.Sleep(3000);
+Console.Clear();
+
+Console.WriteLine("What was the word?");
+string userInput = Console.ReadLine();
+
+if (word == userInput)
+    Console.WriteLine("Correct");
+else
+    Console.WriteLine("Incorrect");
