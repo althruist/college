ac30b50395723b8fe560b3b6f37af2139b61d053
Kieran <73611627+althruist@users.noreply.github.com>
2024-10-10 08:39:11 +0200

    Create Program.cs

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Please input your theory mark:");
+string theoryMark = Console.ReadLine();
+Console.WriteLine("Please input your practical mark:");
+string practicalMark = Console.ReadLine();
+
+int practicalMarkInt = Convert.ToInt32(practicalMark);
+int theoryMarkInt = Convert.ToInt32(theoryMark);
+
+if (practicalMarkInt < 50)
+{
+    Console.WriteLine("You did not pass the practical");
+}
+else
+{
+    Console.WriteLine("You passed the practical!");
+}
+if (theoryMarkInt < 50)
+{
+    Console.WriteLine("You did not pass the theory");
+}
+else
+{
+    Console.WriteLine("You passed the theory!");
+}
+
+if ((theoryMarkInt + practicalMarkInt) >= 125 && theoryMarkInt >= 50 && practicalMarkInt >= 50)
+{
+    Console.WriteLine("You passed the semester!");
+}
+else
+{
+    Console.WriteLine("You did not pass the semester");
+}
