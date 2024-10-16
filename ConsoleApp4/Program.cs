32cc3d2c931f3583a71fa1e5e37ca740513e750d
Kieran <73611627+althruist@users.noreply.github.com>
2024-10-16 10:07:20 +0200

    Create Program.cs

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("How many times?");
+var num = Console.ReadLine();
+var newNum = Convert.ToInt32(num);
+
+for (int i = 1; i <= newNum; i++){
+    Console.Clear();
+    Console.WriteLine(i);
+    Thread.Sleep(i);
+}
+Thread.Sleep(3000);
+
+Console.Clear();
+Console.Write("Please choose a number: ");
+int multiplier = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("MULTIPLE   INCREMENT   ANSWER");
+for (int i = 1; i <= multiplier; i++){
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("|  " + multiplier + "  | * |  " + i + "  | = |  " + multiplier * i + "  |");
+}
